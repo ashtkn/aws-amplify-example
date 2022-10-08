2674c6b3b4ad9c991d29a705da3727e57775aed9
Asahi Takenouchi <asahi.taken@gmail.com>
2022-10-08 11:34:34 +0900

    feat: add authentication flow

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Hello {user?.username}!</p>
+            <a className="App-link" href="." onClick={() => signOut?.()}>
+              Sign Out
+            </a>
+          </header>
+        )}
+      </Authenticator>
     </div>
   );
-}
+};
 
 export default App;
